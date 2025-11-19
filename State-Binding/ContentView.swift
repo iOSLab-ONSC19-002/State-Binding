4c8593f65f89bffc6aefa02b5a7ceb70862de6f2
Sonzzini <paulo.sonzzini@gmail.com>
2025-11-19 18:40:25 -0300

    Initial Commit

---
+++ State-Binding/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  State-Binding
+//
+//  Created by Paulo Sonzzini Ribeiro de Souza on 19/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ State-Binding/State_BindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  State_BindingApp.swift
+//  State-Binding
+//
+//  Created by Paulo Sonzzini Ribeiro de Souza on 19/11/25.
+//
+
+import SwiftUI
+
+@main
+struct State_BindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
